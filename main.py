1a1e5c812317b99c53da188ab4834a15df1d973d
Mensh Nikolay <tony.borbozo@gmail.com>
2024-05-25 00:52:11 +0500

    new main

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def main(directory,num_processed=None):
     images = get_image_files(directory)
     if num_processed is None:
         selected_img = images
-        print(selected_img)
+        
     else:
         selected_img = select_random_images(images=images, num=num_processed)
-        print(selected_img)
+        
     for path in tqdm(selected_img):
         quality_estimator(input_image=path, 
                           sdk_path=r"C:\Users\Mensh\3DiVi_FaceSDK\3_22_0", 
@@ -28,5 +28,5 @@ def main(directory,num_processed=None):
 
 if __name__ == "__main__":
     directory = "C:/Users/Mensh/Desktop/lfw"
-    main(directory=directory, num_processed=10)
+    main(directory=directory, num_processed=100)
     plot_histogram(r'C:\Users\Mensh\Desktop\divi\result.csv')
